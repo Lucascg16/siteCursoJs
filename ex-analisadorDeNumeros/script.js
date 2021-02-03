47514d32d42973565ebb8abb7b68bd157cafaed7
Lucas Carlos Giacomin <luquinha.lcg@gmail.com>
2021-02-03 12:15:55 -0300

    contador de números

---
+++ ex-analisadorDeNumeros/script.js
@@ -0,0 +1,62 @@
+var lista = document.getElementById('lista')
+var vet = []
+var res = document.getElementById('res')
+var num = document.getElementById('num')
+
+function add() {
+    var n = Number(num.value)
+    if (isNumero(num.value) && !inLista(num.value, vet)) {
+        vet.push(n)
+        var item = document.createElement('option')
+        item.text = `O número ${n} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        alert('Número invalido ou ja adicionado')
+    }
+    num.value = ''
+    num.focus()
+}
+function analisar() {
+    if (vet.length != 0) {
+        var soma = 0
+        var maior = vet[0]
+        var menor = vet[0]
+        var media = 0
+        for(var pos in vet){
+            soma += vet[pos]
+            if(vet[pos] > maior){
+                maior = vet[pos]
+            }
+            if(vet[pos] < menor){
+                menor = vet[pos]
+            }
+        }
+        media = soma/vet.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo foram adicionados ${vet.length} números</p>`
+        res.innerHTML += `<p>O maior número foi ${maior}</p>`
+        res.innerHTML += `<p>O menor número foi ${menor}</p>`
+        res.innerHTML += `<p>A soma dos números é ${soma}</p>`
+        res.innerHTML += `<p>A média dos valores é ${media}</p>`
+
+    }else{
+        alert('Adcione um número')
+    }
+}
+
+//funções de funcionamento...
+function isNumero(n) {
+    if (1 <= Number(n) && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
